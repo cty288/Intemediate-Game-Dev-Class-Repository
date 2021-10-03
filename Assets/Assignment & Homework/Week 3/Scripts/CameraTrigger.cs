1b578d71b367016fb1181b193b0061d7e69f38e8
cty288 <cty288@163.com>
2021-10-02 23:19:44 -0400

    d

---
+++ Assets/Assignment & Homework/Week 3/Scripts/CameraTrigger.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrigger : MonoBehaviour {
+    public Vector2 TargetPosition;
+    public float lerp = 0.5f;
+
+    public float size = 5f;
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.name == "Ball") {
+            Week3.CameraControl cameraControl = Camera.main.GetComponent<Week3.CameraControl>();
+
+            cameraControl.TargetPosition = TargetPosition;
+            cameraControl.lerp = lerp;
+            cameraControl.targetSize = size;
+        }
+    }
+}
